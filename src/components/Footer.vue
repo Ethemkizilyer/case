eabc3fdb575b3dcc6b87ff6e767b0cab9cc746b9
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-09-25 16:34:48 +0300

    fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 
 </script>
 <template>
-<div class="bg-dark position-fixed bottom-0 asd">
+<div class="bg-dark  bottom-0 asd">
   <div class="p-4">
     <div class="row">
       <div class="col-6">
